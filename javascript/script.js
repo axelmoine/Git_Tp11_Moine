e0c78a7bd0929e11ffdc03367692da636b5076e3
axelmoine <axel.moine@eleves.institutlemonnier.fr>
2023-01-26 09:49:42 +0100

    debut js

--- javascript/script.js
+++ javascript/script.js
@@ -4,6 +4,7 @@ window.onload=function(){
 
 //texte = document.getElementById('texte')
 }
-function addition(){document.getElementById('texte').innerText="ADDITION"}
-function multiplication(){document.getElementById('texte').innerText="MULTIPLICATION"}
-function effacer(){document.getElementById('texte').innerText=" "}
+
+function add(){document.getElementById('texte').innerText="ADDITION"}
+function mult(){document.getElementById('texte').innerText="MULTIPLICATION"}
+function efface(){document.getElementById('texte').innerText=" "}
