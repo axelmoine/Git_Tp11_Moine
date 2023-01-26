02feb55d23e63dab18b7ba4bd6250d6505e5e059
axelmoine <axel.moine@eleves.institutlemonnier.fr>
2023-01-26 09:31:57 +0100

    test

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+//window.addEventListener("load", alert("charge"));
+
+window.onload=function(){
+
+//texte = document.getElementById('texte')
+}
+function addition(){document.getElementById('texte').innerText="ADDITION"}
+function multiplication(){document.getElementById('texte').innerText="MULTIPLICATION"}
+function effacer(){document.getElementById('texte').innerText=" "}
